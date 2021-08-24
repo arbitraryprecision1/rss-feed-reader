bcf33f72f2fa9cdc949850895ae6dd8e1eb37849
arbitraryprecision1 <66964768+arbitraryprecision1@users.noreply.github.com>
2021-08-24 20:44:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+If there are any issues with `docker-compose up` try `docker-compose pull` first
+
+- localhost:3000 contains gui
+- URI options can be provided 
+    - found at https://github.com/rss-bridge/rss-bridge/wiki/Command-Line-Interface-(CLI)
+    - specific params for a bridge found under PARAMETERS in php files in https://github.com/RSS-Bridge/rss-bridge/tree/master/bridges
---
+++ app/pages/index.js
@@ -0,0 +1,76 @@
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import NavComponent from '../components/Navbar';
+
+
+export default function Home() {
+  const [data, setData] = useState('loading');
+
+  async function getJson() {
+    await fetch(
+      'http://localhost:3000/?action=display&bridge=Twitter&context=By+username&u=3blue1brown&format=Json'
+    )
+    .then(r => r.json())
+    .then(d => setData(d.title))
+    .catch(err => err.toString())
+  }
+
+  useEffect(() => getJson());
+
+  return (
+    <div>
+      <Head>
+        <title>title</title>
+      </Head>
+      <NavComponent />
+
+      <Container>
+        <h1>Some heading</h1>
+        <p>some text</p>
+        <p>{data}</p>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <Card.Header>Card 1</Card.Header>
+              <Card.Body>
+              <Card.Title> Card Title </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+              </Card.Text>
+            </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header>Card 2</Card.Header>
+              <Card.Body>
+              <Card.Title> Card Title </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+              </Card.Text>
+            </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header>Card 3</Card.Header>
+              <Card.Body>
+              <Card.Title> Card Title </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk
+                of the card's content.
+              </Card.Text>
+            </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
